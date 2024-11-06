81cdef9189f3f282cffe37481f7c6881e5586d24
Josh Adler <lexusjosh@gmail.com>
2024-11-06 21:00:35 +1000

    Use parameters

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,17 @@
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
 
+#define MODULES_WIDE 1
+#define MODULES_HIGH 2
+#define WIDTH 8*MODULES_WIDE
+#define HEIGHT 8*MODULES_HIGH
+#define NUMPIXELS WIDTH*HEIGHT
+
 const int neoPixelPin = 15; // control pin
-const int numPixels = 64;	// number of pixels
-unsigned int hue[64] = {0};
+unsigned int hue[NUMPIXELS] = {0};
 
 // set up matrix:
-Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, neoPixelPin,
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(WIDTH, HEIGHT, neoPixelPin,
 											   NEO_MATRIX_TOP + NEO_MATRIX_RIGHT +
 											   NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE,
 											   NEO_GRB + NEO_KHZ800);
@@ -18,7 +23,7 @@ void setup()
 	matrix.begin(); // initialize pixel strip
 	matrix.setBrightness(10);
 	matrix.clear(); // clear pixels
-	for (int thisPixel = 0; thisPixel < numPixels; thisPixel++)
+	for (int thisPixel = 0; thisPixel < NUMPIXELS; thisPixel++)
 		{
       hue[thisPixel] = thisPixel * 100;
 			matrix.setPixelColor(thisPixel, matrix.ColorHSV(hue[thisPixel]));
@@ -29,11 +34,11 @@ void setup()
 
 void loop()
 {
-	for (int thisPixel = 0; thisPixel < numPixels; thisPixel++)
+	for (int thisPixel = 0; thisPixel < NUMPIXELS; thisPixel++)
 		{
 			matrix.setPixelColor(thisPixel, matrix.ColorHSV(hue[thisPixel]));
       hue[thisPixel] = hue[thisPixel] + 50;
 		}
     matrix.show();
-    delay(1);
+    //delay(1);
 }
